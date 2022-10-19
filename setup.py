57e46d9158d0a0aa48f0a1d606dc9fc341e79b0d
Hzzone <zhizhonghwang@gmail.com>
2022-10-19 17:06:10 +0800

    .

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Hzzone/torch_clustering",
-    packages=setuptools.find_packages(),
+    packages=['torch_clustering', ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
