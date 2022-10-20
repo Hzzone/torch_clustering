b70d590fe9874893c1fe68e5252f55a7935b39ca
Hzzone <zhizhonghwang@gmail.com>
2022-10-20 13:36:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Hzzone/torch_clustering",
-    packages=['torch_clustering', ],
+#     packages=['torch_clustering', ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
